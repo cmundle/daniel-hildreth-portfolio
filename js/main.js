041269187c35dd3be39e440f19706887d776078e
Christopher Mundle <christopher.mundle@gmail.com>
2016-05-17 21:38:25 -0400

    Mobile menu rotation fix
    If a human rotates a phone that pulls the medium breakpoint and rotates
    back to small (portrait). The mobile menus would still be open. Using
    Foundation's media query tool, main.js now detects if the device has
    been rotated from the medium breakpoint and closes the menus. Styling
    for small phone menus has also be improved to account for the small
    landscape real estate.

--- js/main.js
+++ js/main.js
@@ -16,3 +16,12 @@ $('.closeButton').on('click', function() {
   $('#socialOverlay').removeClass("open");
   $('body').removeClass("noScroll");
 });
+
+// Close mobile menu if open and screen rotates back from a medium breakpoint
+$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+  if (oldSize = 'medium') {
+    $('#menuOverlay').removeClass("open");
+    $('#socialOverlay').removeClass("open");
+    $('body').removeClass("noScroll");
+  }
+});

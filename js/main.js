6358e09c54b191b1aa4bf9e584970c4c2215f4b9
Christopher Mundle <christopher.mundle@gmail.com>
2019-02-10 16:08:49 -0500

    Detect if page header is visable to adjust fixed nav

--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ var menuButton = document.getElementById('mainMenuButton');
 var nav = document.getElementById('topNav');
 var mainMenu = document.getElementById('mainMenu');
 
+function isVisible(elem) {
+	var y = elem.offsetTop;
+	var height = elem.offsetHeight;
+
+	while ( elem = elem.offsetParent ) {
+		y += elem.offsetTop;
+	}
+
+	var maxHeight = y + height;
+	var visible = ( y < ( window.pageYOffset + window.innerHeight ) ) && ( maxHeight >= window.pageYOffset );
+	return  visible; 
+}
+
+function isHeaderVisible() {
+	var header = document.getElementById('pageHeader');
+
+	if (isVisible(header)){
+		nav.classList.add('white');
+	} else {
+		nav.classList.remove('white');
+	}
+}
+
 window.addEventListener('scroll', function() {
 	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
@@ -13,6 +36,8 @@ window.addEventListener('scroll', function() {
 	}
 	lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 }, false);
+
+window.addEventListener('scroll', isHeaderVisible);
 
 menuButton.addEventListener('click', function() {
 	if (mainMenu.classList.contains('open')) {

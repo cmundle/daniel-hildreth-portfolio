df25f0d3c5f82c8b3014433b354470df98eade62
Christopher Mundle <christopher.mundle@gmail.com>
2019-03-17 22:52:55 -0400

    Refactor mobile and desktop scroll nav treatment

--- js/main.js
+++ js/main.js
@@ -3,43 +3,82 @@ var menuButton = document.getElementById('mainMenuButton');
 var nav = document.getElementById('topNav');
 var mainMenu = document.getElementById('mainMenu');
 
+// Global check if an element visible to the user
 function isVisible(elem) {
 	var y = elem.offsetTop;
 	var height = elem.offsetHeight;
 
-	while ( elem = elem.offsetParent ) {
+	while (elem = elem.offsetParent) {
 		y += elem.offsetTop;
 	}
 
 	var maxHeight = y + height;
-	var visible = ( y < ( window.pageYOffset + window.innerHeight ) ) && ( maxHeight >= window.pageYOffset );
-	return  visible; 
+	var visible = (y < (window.pageYOffset + window.innerHeight)) && (maxHeight >= window.pageYOffset);
+	return visible;
 }
 
+// Is header visible to the user
 function isHeaderVisible() {
 	var header = document.getElementById('pageHeader');
 
-	if (isVisible(header)){
-		nav.classList.remove('fixed');
+	if (isVisible(header)) {
+		nav.classList.remove('dark-bg');
 	} else {
-		nav.classList.add('fixed');
+		nav.classList.add('dark-bg');
 	}
 }
 
-window.addEventListener('scroll', function() {
-	var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-	if (scrollTop < lastScrollTop) {
-		nav.setAttribute('class','show');
-	} else {
-		nav.removeAttribute('class','show');
-	}
-	lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-}, false);
-
+window.onload = function () {
+	isHeaderVisible();
+};
 window.addEventListener('scroll', isHeaderVisible);
 
-menuButton.addEventListener('click', function() {
+// Scroll nav position effect
+(function (document, window, index) {
+	'use strict';
+
+	var elSelector = '.header',
+		element = document.querySelector(elSelector);
+
+	if (!element) return true;
+
+	var elHeight = 0,
+		elTop = 0,
+		dHeight = 0,
+		wHeight = 0,
+		wScrollCurrent = 0,
+		wScrollBefore = 0,
+		wScrollDiff = 0;
+
+	window.addEventListener('scroll', function () {
+		elHeight = element.offsetHeight;
+		dHeight = document.body.offsetHeight;
+		wHeight = window.innerHeight;
+		wScrollCurrent = window.pageYOffset;
+		wScrollDiff = wScrollBefore - wScrollCurrent;
+		elTop = parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+		if (wScrollCurrent <= 0)
+			element.style.top = '0px';
+
+		else if (wScrollDiff > 0)
+			element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+
+		else if (wScrollDiff < 0) {
+			if (wScrollCurrent + wHeight >= dHeight - elHeight)
+				element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight) < 0 ? elTop : 0) + 'px';
+
+			else
+				element.style.top = (Math.abs(elTop) > elHeight ? -elHeight : elTop) + 'px';
+		}
+
+		wScrollBefore = wScrollCurrent;
+	});
+
+}(document, window, 0));
+
+// When mobile/tablet menu button is pressed
+menuButton.addEventListener('click', function () {
 	if (mainMenu.classList.contains('open')) {
 		mainMenu.removeAttribute('class', 'open');
 		menuButton.innerText = "menu";
